7f4d0415772b6b8c1ecfde91c366420b42006896
OfficialChutney <noav@itu.dk>
2025-09-18 12:57:55 +0200

    Had an IDE issue where it showed there was code in a file IDatabaseRepository, but it was ghost code, and there was no code in it. It has been fixed. Missing dll file is also included.

--- SimpleDB/IDatabaseRepository.cs
+++ SimpleDB/IDatabaseRepository.cs
@@ -3,10 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
-
-namespace SimpleDB
-{
-    internal class IDatabaseRepository
-    {
+namespace SimpleDB { 
+    public interface IDatabaseRepository<T> { 
+        public IEnumerable<T> Read(int? limit = null);
+        public void Store(T record); 
     }
 }
