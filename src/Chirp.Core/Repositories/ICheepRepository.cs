7c9023f09bb486de2359aa9c81eddb2983697b97
OfficialChutney <noav@itu.dk>
2025-11-03 14:43:12 +0100

    Fixed issues that produces warnings on build

--- src/Chirp.Core/Repositories/ICheepRepository.cs
+++ src/Chirp.Core/Repositories/ICheepRepository.cs
@@ -7,8 +7,8 @@ namespace Chirp.Core.Repositories
         IEnumerable<CheepDTO> GetAll(int page = 1, int pageSize = 32);
         IEnumerable<CheepDTO> GetByAuthor(string authorName, int page = 1, int pageSize = 32);
         public void CreateNewAuthor(string name, string email, string password);
-        public AuthorDTO FindAuthorByName(string name);
-        public AuthorDTO FindAuthorByEmail(string email);
+        public AuthorDTO? FindAuthorByName(string name);
+        public AuthorDTO? FindAuthorByEmail(string email);
         public void AddChirp(CheepDTO chirp);
         void Save();
     }
