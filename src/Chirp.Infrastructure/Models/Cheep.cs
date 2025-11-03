95103e202b8fe67245e88db0524dc9de8dd3f932
gilbert400 <alfd@itu.dk>
2025-11-03 15:27:04 +0100

    Added changes for Cheep.cs to require that the max length of the text is 160 chars. Also added migration for the database changes.

--- src/Chirp.Infrastructure/Models/Cheep.cs
+++ src/Chirp.Infrastructure/Models/Cheep.cs
@@ -1,10 +1,14 @@
-﻿namespace Chirp.Infrastructure.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Chirp.Infrastructure.Models
 {
     public class Cheep
     {
         public int CheepId { get; set; }                  // PK from dump
         public int AuthorId { get; set; }                 // FK -> Author.AuthorId
         public Author Author { get; set; } = null!;
+        [Required]
+        [StringLength(160)]
         public string Text { get; set; } = null!;
         public DateTime TimeStamp { get; set; }
     }
