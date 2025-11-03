c40b6eee15b25dc5e87bdc782c17c70e608d8324
gilbert400 <alfd@itu.dk>
2025-11-03 17:30:39 +0100

    oauth and dotnet secrets implemented

--- src/Chirp.Web/Program.cs
+++ src/Chirp.Web/Program.cs
@@ -1,37 +1,48 @@
- using Chirp.Core.Repositories;
+using Chirp.Core.Repositories;
 using Chirp.Core.Services;
 using Chirp.Infrastructure.Data;
 using Chirp.Razor.Repositories;
 using Microsoft.EntityFrameworkCore;
+using Microsoft.AspNetCore.Authentication.Cookies;
+using AspNet.Security.OAuth.GitHub;
 
 var builder = WebApplication.CreateBuilder(args);
 
-// Add services to the container.
 builder.Services.AddRazorPages();
 builder.Services.AddScoped<ICheepService, CheepService>();
 builder.Services.AddScoped<ICheepRepository, CheepRepository>();
 
-
-// Load database connection via configuration
 string? connectionString = builder.Configuration.GetConnectionString("DefaultConnection");
 builder.Services.AddDbContext<ChirpDBContext>(options => options.UseSqlite(connectionString));
+
+builder.Services
+    .AddAuthentication(options =>
+    {
+        options.DefaultScheme = CookieAuthenticationDefaults.AuthenticationScheme;
+        options.DefaultChallengeScheme = GitHubAuthenticationDefaults.AuthenticationScheme;
+    })
+    .AddCookie()
+    .AddGitHub(options =>
+    {
+        options.ClientId = builder.Configuration["Authentication:GitHub:ClientId"];
+        options.ClientSecret = builder.Configuration["Authentication:GitHub:ClientSecret"];
+        options.CallbackPath = new PathString("/signin-github");
+    });
+
+builder.Services.AddAuthorization();
 
 var app = builder.Build();
 
 using (var scope = app.Services.CreateScope())
 {
     var ctx = scope.ServiceProvider.GetRequiredService<ChirpDBContext>();
-
     ctx.Database.Migrate();
     DbInitializer.SeedDatabase(ctx);
 }
 
-//
-// Configure the HTTP request pipeline.
 if (!app.Environment.IsDevelopment())
 {
     app.UseExceptionHandler("/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
     app.UseHsts();
 }
 
@@ -39,6 +50,9 @@ app.UseHttpsRedirection();
 app.UseStaticFiles();
 
 app.UseRouting();
+
+app.UseAuthentication();
+app.UseAuthorization();
 
 app.MapRazorPages();
 
