2fa6b770aab6ba7928d64994f30b34da8410772b
gilbert400 <alfd@itu.dk>
2025-11-03 16:46:56 +0100

    removed again

---
+++ src/Chirp.Infrastructure/Migrations/20251103153948_RemovedPasswordColumn.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class RemovedPasswordColumn : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "Password",
+                table: "Authors");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Password",
+                table: "Authors",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "");
+        }
+    }
+}
