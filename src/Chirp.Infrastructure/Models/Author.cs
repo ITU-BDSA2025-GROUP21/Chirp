071833f7ac1d189864f73ca42703b3351638a4f9
gilbert400 <alfd@itu.dk>
2025-11-03 16:46:15 +0100

    removed password column

--- src/Chirp.Infrastructure/Models/Author.cs
+++ src/Chirp.Infrastructure/Models/Author.cs
@@ -4,7 +4,6 @@
     {
         public int AuthorId { get; set; }                 // PK from dump
         public string Name { get; set; } = null!;
-        public string Password { get; set; } = null!;
         public string Email { get; set; } = null!;
         public List<Cheep> Cheeps { get; set; } = new();
     }
