454c6d27173d85c036767f2c5512105625123619
OfficialChutney <noav@itu.dk>
2025-11-13 17:59:08 +0100

    Fixed EFCore issue

---
+++ src/Chirp.Infrastructure/Migrations/20251113165726_FKAuthorMigrate.cs
@@ -0,0 +1,93 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Chirp.Infrastructure.Migrations
+{
+    /// <inheritdoc />
+    public partial class FKAuthorMigrate : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeps_Authors_AuthorId",
+                table: "Cheeps");
+
+            migrationBuilder.DropTable(
+                name: "Authors");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "AuthorId",
+                table: "Cheeps",
+                type: "TEXT",
+                nullable: false,
+                oldClrType: typeof(int),
+                oldType: "INTEGER");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: false,
+                defaultValue: "",
+                oldClrType: typeof(string),
+                oldType: "TEXT",
+                oldNullable: true);
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeps_AspNetUsers_AuthorId",
+                table: "Cheeps",
+                column: "AuthorId",
+                principalTable: "AspNetUsers",
+                principalColumn: "Id",
+                onDelete: ReferentialAction.Cascade);
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Cheeps_AspNetUsers_AuthorId",
+                table: "Cheeps");
+
+            migrationBuilder.AlterColumn<int>(
+                name: "AuthorId",
+                table: "Cheeps",
+                type: "INTEGER",
+                nullable: false,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.AlterColumn<string>(
+                name: "Name",
+                table: "AspNetUsers",
+                type: "TEXT",
+                nullable: true,
+                oldClrType: typeof(string),
+                oldType: "TEXT");
+
+            migrationBuilder.CreateTable(
+                name: "Authors",
+                columns: table => new
+                {
+                    AuthorId = table.Column<int>(type: "INTEGER", nullable: false)
+                        .Annotation("Sqlite:Autoincrement", true),
+                    Email = table.Column<string>(type: "TEXT", nullable: false),
+                    Name = table.Column<string>(type: "TEXT", nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Authors", x => x.AuthorId);
+                });
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Cheeps_Authors_AuthorId",
+                table: "Cheeps",
+                column: "AuthorId",
+                principalTable: "Authors",
+                principalColumn: "AuthorId",
+                onDelete: ReferentialAction.Cascade);
+        }
+    }
+}
--- src/Chirp.Infrastructure/Models/Cheep.cs
+++ src/Chirp.Infrastructure/Models/Cheep.cs
@@ -1,10 +1,12 @@
 ﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 
 namespace Chirp.Infrastructure.Models
 {
     public class Cheep
     {
         public int CheepId { get; set; }
+        [ForeignKey(nameof(Author))]
         public string AuthorId { get; set; } = null!;
         public Author Author { get; set; } = null!;
         [Required]
