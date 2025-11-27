4d36af45125ea77281ddf0dc38509a20387a9b7d
aold <alfredsoldin@gmail.com>
2025-11-27 12:40:49 +0100

    Moved 'CreatCheepAndFilterAuthor' to unittest1

--- test/xUnitTests/UnitTest1.cs
+++ test/xUnitTests/UnitTest1.cs
@@ -1,6 +1,10 @@
 using Chirp.Core.DTO;
 using Chirp.Core.Services;
 using Xunit.Abstractions;
+using Chirp.Core.Repositories;
+{
+    
+}
 
 namespace XunitTests
 {
@@ -8,11 +12,13 @@ namespace XunitTests
     {
         private readonly ITestOutputHelper _output;
         private readonly ICheepService _cheepService;
+        private readonly ICheepRepository _cheepRepository;
 
 
         public UnitTest1(ITestOutputHelper output, TestServices testService)
         {
             _output = output;
+            _cheepRepository = testService.CheepRepository;
             _cheepService = testService.CheepService;
         }
 
@@ -104,6 +110,32 @@ namespace XunitTests
             }
 
             Assert.True(ordered);
+        }
+
+        public void CreateCheepAndFilterAuthor()
+        {
+            var Author = new AuthorDTO()
+            {
+                Name = "Testing Client",
+                Email = "Tester@Cheep.com",
+                Password = "TestPassword"
+            };
+
+            var Chirp = new CheepDTO()
+            {
+                Author = Author.Name,
+                Message = "Test Message",
+                CreatedDate = "2023-08-01 13:15:37",
+            };
+
+            _cheepRepository.CreateNewAuthor(Author.Name, Author.Email);
+            _cheepRepository.AddChirp(Chirp);
+
+            var cheeps = _cheepService.GetCheepsFromAuthor(Author.Name);
+
+            Assert.Single(cheeps);
+            Assert.Equal(Author.Name, cheeps.First().Author);
+            Assert.Equal(Chirp.Message, cheeps.First().Message);
         }
     }
 }
