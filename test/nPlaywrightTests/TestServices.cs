aabd918a940136e4c3b58dfdcc0c2b56a655a0d1
Quorning-DK <philip.quorning@live.com>
2025-12-04 14:35:26 +0100

    initial playwright setup done, though problems with CreateClient persists

---
+++ test/nPlaywrightTests/TestServices.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.Playwright;
+
+using Chirp.Web;
+
+namespace nPlaywrightTests
+{
+    public class TestChirpWebFactory : WebApplicationFactory<Program>
+    {
+
+        protected override void ConfigureWebHost(IWebHostBuilder builder)
+        {
+            base.ConfigureWebHost(builder);
+
+            // apply db and other stuff.
+        }
+        
+
+    }
+}
---
+++ test/nPlaywrightTests/UnitTest1.cs
@@ -0,0 +1,33 @@
+﻿using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Microsoft.Playwright;
+using Microsoft.Playwright.NUnit;
+using NUnit.Framework;
+
+namespace nPlaywrightTests;
+
+[TestFixture]
+public class Tests
+{
+    private TestChirpWebFactory _factory = null!;
+
+    [OneTimeSetUp]
+    public void SetupFactory()
+    {
+        _factory = new TestChirpWebFactory();
+        _factory.CreateClient();
+    }
+
+    [Test]
+    public async Task ChirpWebsiteExists()
+    {
+
+    }
+
+    [OneTimeTearDown]
+    public void TearDownFactory()
+    {
+        _factory.Dispose();
+    }
+
+}
