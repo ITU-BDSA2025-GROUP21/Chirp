8ce9c7a49e4a12c7085a34da115b2dcbda74127c
SowzzG <antonthejsen@gmail.com>
2025-12-04 12:45:32 +0100

    Start of Tests
    UI & E2E

---
+++ test/xPlaywrightTests/E2ETests.cs
@@ -0,0 +1,10 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace PlaywrightTests;
+
+public class E2ETests : PageTest
+{
+    
+}
---
+++ test/xPlaywrightTests/UITests.cs
@@ -0,0 +1,16 @@
+using System.Text.RegularExpressions;
+using Microsoft.Playwright;
+using Microsoft.Playwright.Xunit;
+
+namespace PlaywrightTests;
+
+public class UiTests : IClassFixture<PlaywrightFixture>
+{
+    [Fact]
+    public void CheepInputShowsWhenAuthed()
+    {
+        // Assumes that we can create and login.
+        
+        
+    }
+}
