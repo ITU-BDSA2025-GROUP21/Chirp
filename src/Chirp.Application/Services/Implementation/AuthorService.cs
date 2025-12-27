b6dcc9b7941cb2c7134cda671c8611146288a56f
gilbert400 <alfd@itu.dk>
2025-12-27 12:36:52 +0100

    merged main forreal this time

--- src/Chirp.Application/Services/Implementation/AuthorService.cs
+++ src/Chirp.Application/Services/Implementation/AuthorService.cs
@@ -2,7 +2,6 @@ using Chirp.Application.DTO;
 using Chirp.Application.Services.Interface;
 using Chirp.Core.Models;
 using Chirp.Core.Repositories;
-using Chirp.Core.Services;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using System.Reflection.Metadata;
@@ -31,7 +30,7 @@ namespace Chirp.Application.Services.Implementation
                 Name = author.Name,
                 karma = author.karma,
                 Email = author.Email == null ? "noEmaiFound@nomail.dk" : author.Email,
-                CreationDate = author.CreationDate.ToString("dd/MM/yyyy HH:mm")
+                CreationDate = author.CreationDate.ToString("dd/MM/yyyy HH:mm"),
                 ProfilePicPath = author.ProfilePicPath
             };
         }
