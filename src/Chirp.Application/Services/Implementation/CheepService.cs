ce72d1c74649d33c86efae2f25c7e857026ed1ef
UngBalle2501 <andjo@itu.dk>
2025-12-29 14:21:07 +0100

    Added Documentation for Interface CheepService

--- src/Chirp.Application/Services/Implementation/CheepService.cs
+++ src/Chirp.Application/Services/Implementation/CheepService.cs
@@ -85,7 +85,7 @@ namespace Chirp.Application.Services.Implementation
         /// </summary>
         /// <remarks>Returns null if no cheep exists with the specified parameter <paramref name="cheepId"/>.</remarks>
         /// <param name="cheepId">The unique identifier of the cheep to retrieve. Must be a positive integer.</param>
-        /// <returns>A <see cref="CheepDTO"/> containing the details of the cheep if found; otherwise, <see langword="null"/>.</returns>
+        /// <returns>A <see cref="CheepDTO"/> containing the details of the cheep if found; otherwise, null/>.</returns>
         public CheepDTO? GetById(int cheepId)
         {
             return _cheepRepository.GetById(cheepId) is Cheep cheep ? CreateCheepDTO(cheep) : null;
